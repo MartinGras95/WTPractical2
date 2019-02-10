96f9ff13089eb0201c1766664432451b0e0ade47
Martin <martin.gras95@gmail.com>
2019-02-10 16:03:22 +0000

    Random damage taken by enemies added

--- JSlessons/HellCrawl/script.js
+++ JSlessons/HellCrawl/script.js
@@ -1,9 +1,16 @@
 var name = "Player";
 var hp = 100;
+var str = 10;
+
 var treasures = ["Gold","Diamonds","a Spirit Orb","a Ruby","an Hourglass","a Goblet","Nothing"];
 var enemies = ["Goblin","Undead Mammoth","Skeleton","Warlock","Troll","Cyclops","Wolf"];
 
+// Enemy stats
 var currEnemy = "";
+var enemyHp = 10;
+var enemyStr = [2,3,4,5];
+
+
 var currTreasure = "";
 
 
@@ -24,18 +31,29 @@ function getRandomEnemy(){
   return enemies[num];
 }
 
+// Random Enemy damage
+
+function getRandomEnemyDamage(){
+  var num = Math.round(Math.random()*(0+enemyStr.length-1)+0);
+  hp = hp - enemyStr[num];
+  return enemyStr[num];
+  hp = hp - enemyStr[num];
+}
+
 
 
 
 function roomOneLookAround(){
   document.getElementById("roomOneDesc").innerHTML = "You found " + getRandomTreasure();
-  document.getElementById("roomOneDesc").innerHTML += "<p> You were attacked by a </p> " +getRandomEnemy();
+  document.getElementById("roomOneDesc").innerHTML += "<p> You were attacked by a </p> " +getRandomEnemy() +" with 10 hp";
   document.getElementById("commandKill").style.visibility= "visible";
 }
 
 function roomOneAttack(){
-  document.getElementById("roomOneDesc").innerHTML = "You hit and killed the " +currEnemy;
-  document.getElementById("roomOneDesc").innerHTML += "<p>You pick up </p> " + currTreasure;
+  document.getElementById("roomOneDesc").innerHTML = "<p> The "+currEnemy +" hit you for  " +getRandomEnemyDamage() + " hp</p>"
+  document.getElementById("roomOneDesc").innerHTML += "<p>You remaining hp = " + hp; "</p>"
+  document.getElementById("roomOneDesc").innerHTML += "You hit the " +currEnemy + " for " + str + " hp";
+  document.getElementById("roomOneDesc").innerHTML += "<p>You pick up " + currTreasure; + "</p>"
   document.getElementById("commandKill").style.visibility = "hidden";
   document.getElementById("commandLookAround").style.visibility = "hidden";
 }
